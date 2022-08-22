80419ce9db931544875daf3975dd4d20057ce770
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-22 12:02:41 +0300

    Created BackupOutputDirectory

---
+++ src/BackupTool/Backup/BackupOutputDirectory.cs
@@ -0,0 +1,27 @@
+namespace BackupTool.Backup;
+
+internal sealed class BackupOutputDirectory
+{
+    public readonly string OuterDirectory;
+
+    private BackupOutputDirectory(string outerDirectory) => OuterDirectory = outerDirectory;
+
+    public static BackupOutputDirectory FromString(string outerDirectory, Func<string, bool>? createIfNotFound = null)
+    {
+        if (Directory.Exists(outerDirectory) == false)
+        {
+            if (createIfNotFound is not null && createIfNotFound(outerDirectory))
+            {
+                Directory.CreateDirectory(outerDirectory);
+            }
+            else
+            {
+                throw new DirectoryNotFoundException($"Output directory {outerDirectory} doest not exist");
+            }
+        }
+
+        return new(outerDirectory);
+    }
+
+    public static implicit operator string(BackupOutputDirectory directory) => directory.OuterDirectory;
+}
