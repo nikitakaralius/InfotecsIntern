13d189a8086343a2650829e8ca604ac09fe48c6a
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-22 11:31:34 +0300

    Added configuration loading

--- src/BackupTool/Program.cs
+++ src/BackupTool/Program.cs
@@ -1,3 +1,20 @@
 ﻿Log.Logger = new LoggerConfiguration()
              .WriteTo.Console()
              .CreateLogger();
+
+string configurationPath = EnsureCorrectPath(firstLookAt: "./configuration.json");
+
+IConfiguration configuration = new JsonConfiguration(configurationPath);
+
+AppConfiguration appConfiguration;
+
+try
+{
+    appConfiguration = await configuration.LoadAsync<AppConfiguration>();
+}
+catch (Exception e)
+{
+    Log.Error("Unable to load configuration: {Error}", e.Message);
+    return;
+}
+
