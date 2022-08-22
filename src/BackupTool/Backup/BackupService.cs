3a34af49e0847a7d97c79cdb2f669a5635069a1f
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-22 12:31:28 +0300

    Created backup service

---
+++ src/BackupTool/Backup/BackupService.cs
@@ -0,0 +1,47 @@
+namespace BackupTool.Backup;
+
+internal sealed class BackupService
+{
+    public enum CompletionStatus
+    {
+        Success,
+        DoneWithErrors,
+        Canceled
+    }
+
+    private readonly BackupSourceDirectory[] _sourceDirectories;
+    private readonly BackupOutputDirectory _outputDirectory;
+
+    public BackupService(BackupSourceDirectory[] sourceDirectories, BackupOutputDirectory outputDirectory)
+    {
+        _sourceDirectories = sourceDirectories;
+        _outputDirectory = outputDirectory;
+    }
+
+    public CompletionStatus Execute()
+    {
+        var status = CompletionStatus.Success;
+
+        foreach (var sourceDirectory in _sourceDirectories)
+        {
+            try
+            {
+                sourceDirectory.CopyTo(_outputDirectory, (file, exception) =>
+                {
+                    Log.Error("Unable to copy {File}: {Error}", file.FullName, exception.Message);
+                    Log.Information("Do you want to skip this file? {Yes} / {No}", "[Y]es", "[N]o");
+                    status = CompletionStatus.DoneWithErrors;
+                    return Input() == "y";
+                });
+            }
+            catch (Exception)
+            {
+                Log.Information("Deleting Output Directory...");
+                _outputDirectory.Delete();
+                return CompletionStatus.Canceled;
+            }
+        }
+
+        return status;
+    }
+}
