9aaec09e518f8f3546367256794af7cfc6398b9f
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-22 11:36:34 +0300

    Created BackupSourceDirectory

---
+++ src/BackupTool/Backup/BackupSourceDirectory.cs
@@ -0,0 +1,20 @@
+namespace BackupTool.Backup;
+
+internal sealed class BackupSourceDirectory
+{
+    public readonly string Path;
+
+    private BackupSourceDirectory(string path) => Path = path;
+
+    public static BackupSourceDirectory FromString(string path)
+    {
+        if (Directory.Exists(path) == false)
+        {
+            throw new DirectoryNotFoundException($"Directory {path} doest not exist");
+        }
+
+        return new BackupSourceDirectory(path);
+    }
+
+    public static implicit operator string(BackupSourceDirectory directory) => directory.Path;
+}
