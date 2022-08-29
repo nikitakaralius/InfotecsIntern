5b48d3917c6dd064a7029a3cd555aee3e3c31cc9
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-29 16:00:37 +0300

    Implemented feed base logic

--- src/RssAggregator/client-app/src/components/feed/Feed.tsx
+++ src/RssAggregator/client-app/src/components/feed/Feed.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Feed.module.scss';
+import {FetchFeed} from '../../domain/actions';
+import {FeedSource, IProxy, SortedFeed} from '../../domain/compoundTypes';
+import FeedItem from '../feed-item/FeedItem';
 
 interface IFeedProps {
-
+  feedSource: FeedSource;
+  fetchFeed: FetchFeed;
+  proxy: IProxy | null;
 }
 
-const Feed = (props: IFeedProps) => {
+const Feed = ({feedSource, fetchFeed, proxy}: IFeedProps) => {
+  const [feed, setFeed] = useState<SortedFeed>([]);
+
+  useEffect(() => {
+    fetchFeed(feedSource, proxy)
+      .then(f => setFeed(f));
+  }, []);
+
   return (
     <div className={styles.container}>
-      Feed
+      {
+        feed.map(item => <FeedItem item={item} />)
+      }
     </div>
   );
 };
