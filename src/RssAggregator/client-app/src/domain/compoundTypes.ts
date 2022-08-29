b02794786fa98eaaf89b22e4d1721c64dce334d2
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-29 12:31:55 +0300

    Created rest of the domain types

--- src/RssAggregator/client-app/src/domain/compoundTypes.ts
+++ src/RssAggregator/client-app/src/domain/compoundTypes.ts
@@ -6,3 +6,23 @@ export interface IFeedItem {
   link: URL;
   pubDate: Date;
 }
+
+export interface IFeed {
+  title: String50;
+  link: URL;
+}
+
+export type SortedFeedStream = IFeedItem[];
+
+export interface IProxy {
+  host: URL;
+  port: number;
+  username: string;
+  password: string;
+}
+
+export type ConnectionStatus = 'Success' | 'Failure';
+
+export type fetchFeed = (feed: IFeed, proxy: IProxy | null) => SortedFeedStream;
+
+export type connectToProxy = (proxy: IProxy) => ConnectionStatus;
--- src/RssAggregator/client-app/src/domain/simpleTypes.ts
+++ src/RssAggregator/client-app/src/domain/simpleTypes.ts
@@ -1,7 +1,7 @@
 const createClamped = <T>(content: string, n: number, constructor: (content: string) => T) => {
   const clamped = content.substring(0, 100) + '...';
   return constructor(clamped);
-}
+};
 
 export class String50 {
   readonly content: string;
@@ -14,7 +14,6 @@ export class String50 {
     return createClamped<String100>(content, 50, c => new String50(c));
   }
 }
-
 
 export class String100 {
   readonly content: string;
