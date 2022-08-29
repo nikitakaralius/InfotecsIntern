8754d13649a1149edfb9bfce1c1dfd85ff9e76cd
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-29 13:59:19 +0300

    Uninstalled ChakraUI

--- src/RssAggregator/client-app/src/App.tsx
+++ src/RssAggregator/client-app/src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/header/Header';
+import Feed from './components/feed/Feed';
+import FeedStreamList from './components/feed-streams/FeedStreamList';
 
 const App = () => {
   return (
     <div>
-      Hello, World!
+      <Header />
+      <Feed />
+      <FeedStreamList />
     </div>
-  )
-}
+  );
+};
 
 export default App;
