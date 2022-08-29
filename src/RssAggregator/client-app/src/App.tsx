f3c90fc8e94e303fce23bbb9c55e50d158a77529
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-29 11:32:41 +0300

    Fixed Imports

--- src/RssAggregator/client-app/src/App.tsx
+++ src/RssAggregator/client-app/src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-
+    <div>
+      Hello, World!
     </div>
-  );
+  )
 }
 
 export default App;
