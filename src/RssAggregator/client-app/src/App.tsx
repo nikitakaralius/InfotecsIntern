9cd83db112c1a405bf15d1f2d372eadc79980f73
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-29 14:09:27 +0300

    Added content grid

--- src/RssAggregator/client-app/src/App.tsx
+++ src/RssAggregator/client-app/src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from './components/header/Header';
 import Feed from './components/feed/Feed';
 import FeedStreamList from './components/feed-streams/FeedStreamList';
+import './index.css';
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Feed />
-      <FeedStreamList />
+      <div className="content">
+        <Feed />
+        <FeedStreamList />
+      </div>
     </div>
   );
 };
