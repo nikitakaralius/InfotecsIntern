89283404787200b20251a1a1a85b8b98a89ddb0e
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-30 21:09:56 +0300

    Created FetchError type and changed string50 to string80

--- src/RssAggregator/client-app/src/domain/simpleTypes.ts
+++ src/RssAggregator/client-app/src/domain/simpleTypes.ts
@@ -1,9 +1,10 @@
 const createClamped = <T>(content: string, n: number, constructor: (content: string) => T) => {
-  const clamped = content.substring(0, 100) + '...';
-  return constructor(clamped);
+  return content.length <= n
+    ? constructor(content)
+    : constructor(content.substring(0, n - 3) + '...');
 };
 
-export class String50 {
+export class String80 {
   readonly content: string;
 
   private constructor(content: string) {
@@ -11,7 +12,7 @@ export class String50 {
   }
 
   static createClamped(content: string) {
-    return createClamped<String100>(content, 50, c => new String50(c));
+    return createClamped<String100>(content, 80, c => new String80(c));
   }
 }
 
@@ -28,3 +29,9 @@ export class String100 {
 }
 
 export type ConnectionStatus = 'Success' | 'Failure';
+
+export interface NetworkError {
+  message: unknown;
+}
+
+export type FetchError = null | NetworkError | 'Unknown RSS dialect';
