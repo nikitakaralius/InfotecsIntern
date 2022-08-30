7d2b09bc2fba8438f8d3339964d0d8bea97aef2c
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-30 22:03:44 +0300

    Pased stream props

--- src/RssAggregator/client-app/src/components/app/App.tsx
+++ src/RssAggregator/client-app/src/components/app/App.tsx
@@ -3,14 +3,14 @@ import Header from '../header/Header';
 import Feed from '../feed/Feed';
 import FeedStreamList from '../feed-stream-list/FeedStreamList';
 import styles from './App.module.scss';
-import {feedSource} from '../../seedData';
+import {feedSource, streams} from '../../store';
 
 const App = () => {
   return (
     <div className={styles.grid}>
       <Header />
       <Feed feedSource={feedSource} proxy={null}/>
-      <FeedStreamList />
+      <FeedStreamList streams={streams} />
     </div>
   );
 };
