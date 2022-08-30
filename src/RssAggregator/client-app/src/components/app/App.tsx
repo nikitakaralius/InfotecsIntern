be4dc100b3ebd1235f6a5c2297d784c0a01066f5
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-30 21:38:08 +0300

    Changed FeedStreamList directory name

--- src/RssAggregator/client-app/src/components/app/App.tsx
+++ src/RssAggregator/client-app/src/components/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../header/Header';
 import Feed from '../feed/Feed';
-import FeedStreamList from '../feed-streams/FeedStreamList';
+import FeedStreamList from '../feed-stream-list/FeedStreamList';
 import styles from './App.module.scss';
 import {feedSource} from '../../seedData';
 
