a0546e823d64b17c7dd5a278a14b1c10e5739a6e
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-31 23:52:48 +0300

    Styled RssForm

--- src/RssAggregator/client-app/src/components/header/rssForm/RssForm.tsx
+++ src/RssAggregator/client-app/src/components/header/rssForm/RssForm.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {useTypedDispatch, useTypedSelector} from '../../../hooks';
 import {INewFeedStream} from '../../../domain';
 import {appendStream} from '../../../store';
+import styles from './RssForm.module.scss';
 
 const RssForm = () => {
   const [link, setLink] = useState('');
@@ -18,13 +19,15 @@ const RssForm = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} className={styles.container}>
       <input
+        className={styles.input}
         type="url"
         placeholder="RSS Link"
         onChange={i => setLink(i.target.value)}
+        value={link}
       />
-      <input type="submit" value="Add" />
+      <input type="submit" value="Add" className={styles.submit}/>
     </form>
   );
 };
