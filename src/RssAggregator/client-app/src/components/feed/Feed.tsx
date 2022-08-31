a43ad1517b6e7c388cec5104bd8ea7bc3b57b17b
nikitakaralyus <nikitakaralyus@gmail.com>
2022-09-01 00:40:15 +0300

    Style improves (idk what to write here)

--- src/RssAggregator/client-app/src/components/feed/Feed.tsx
+++ src/RssAggregator/client-app/src/components/feed/Feed.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import {useFeed} from '../../hooks';
 import {Article} from './article/Article';
-import styles from './Feed.module.css';
+import styles from './Feed.module.scss';
 
 const Feed = () => {
   const {articles} = useFeed();
 
+  if (articles.content.length == 0)
+    return <div></div>
+
   return (
     <div className={styles.container}>
-      {
-        articles.content.map(a => <Article content={a} key={a.link}/>)
-      }
+      <div className={styles.heading}>
+        Feed
+      </div>
+      <div className={styles.list}>
+        {
+          articles.content.map(a => <Article content={a} key={a.link}/>)
+        }
+      </div>
     </div>
   );
 };
