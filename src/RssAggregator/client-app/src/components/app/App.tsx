b649208345f731657973c27ff9119f0d5f99108b
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-31 20:37:54 +0300

    Cleaned App component up

--- src/RssAggregator/client-app/src/components/app/App.tsx
+++ src/RssAggregator/client-app/src/components/app/App.tsx
@@ -1,29 +1,9 @@
 import React from 'react'
-import {useFeed, useTypedDispatch} from '../../hooks';
-import {INewFeedStream} from '../../domain';
-import {appendStream} from '../../store';
 
 const App = () => {
-
-  const {articles} = useFeed();
-  const dispatch = useTypedDispatch();
-
   return (
     <div>
-      <button onClick={() => {
-        const stream: INewFeedStream = {
-          link: 'https://habr.com/ru/rss/interesting/'
-        }
-        dispatch(appendStream(stream));
-      }}>
-        Add RSS
-      </button>
 
-      <div>
-        {
-          articles.content.map(a => <div>{a.title}</div>)
-        }
-      </div>
     </div>
   )
 }
