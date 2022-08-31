f53624ec88c67ae69ce570283a5b59aaa99a488b
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-31 22:32:30 +0300

    Scaffold App component

--- src/RssAggregator/client-app/src/components/app/App.tsx
+++ src/RssAggregator/client-app/src/components/app/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Header} from '../header';
+import {Feed} from '../feed';
+import {StreamList} from '../streams';
 
 const App = () => {
   return (
     <div>
-
+      <Header />
+      <Feed />
+      <StreamList />
     </div>
   )
 }
