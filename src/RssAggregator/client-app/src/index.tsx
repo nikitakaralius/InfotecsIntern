367d3c87bed6c3f9f6ad85a175a5006d334fe9a0
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-31 17:29:37 +0300

    Wrapped App in Provider

--- src/RssAggregator/client-app/src/index.tsx
+++ src/RssAggregator/client-app/src/index.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import './styles/index.scss';
+import {Provider} from 'react-redux';
+import {setupStore} from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const store = setupStore();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>
 );
