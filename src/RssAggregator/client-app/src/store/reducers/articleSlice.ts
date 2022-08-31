1a1ba46564df7d084da75f38d90f3d0ce9d4c443
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-31 12:27:58 +0300

    Created Article Slice

--- src/RssAggregator/client-app/src/store/reducers/articleSlice.ts
+++ src/RssAggregator/client-app/src/store/reducers/articleSlice.ts
@@ -1,21 +1,38 @@
-import {IArticle} from '../../domain';
-import {createSlice} from '@reduxjs/toolkit';
+import {SortedFeed} from '../../domain';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 interface IFeedState {
-  feed: IArticle[];
+  feed: SortedFeed;
   isLoading: boolean;
   error: string | null;
 }
 
 const initialState: IFeedState = {
-  feed: [],
+  feed: SortedFeed.empty(),
   isLoading: false,
   error: null
-}
+};
 
-const slice = createSlice({
+const articleSlice = createSlice({
   name: 'article',
   initialState: initialState,
   reducers: {
+    feedFetching(state) {
+      state.isLoading = true;
+    },
+
+    feedFetchingSuccess(state, action: PayloadAction<SortedFeed>) {
+      state.isLoading = false;
+      state.error = null;
+      state.feed = action.payload;
+    },
+
+    feedFetchingError(state, action: PayloadAction<string>) {
+      state.isLoading = false;
+      state.error = action.payload;
+      state.feed = SortedFeed.empty();
+    }
   }
-})
+});
+
+export {articleSlice};
