2b7376d92d83c93564bf6b8e3fc2bdf73b3fb808
nikitakaralyus <nikitakaralyus@gmail.com>
2022-09-01 00:22:49 +0300

    Improved StreamList styles

--- src/RssAggregator/client-app/src/components/streams/StreamList.tsx
+++ src/RssAggregator/client-app/src/components/streams/StreamList.tsx
@@ -6,11 +6,19 @@ import styles from './StreamList.module.scss';
 const StreamList = () => {
   const {storage} = useTypedSelector(state => state.stream);
 
+  if (storage.streams.length == 0)
+    return <div></div>
+
   return (
     <div className={styles.container}>
-      {
-        storage.streams.map(s => <Stream content={s} key={s.link}/>)
-      }
+      <div className={styles.heading}>
+        RSS Feed Streams
+      </div>
+      <div className={styles.list}>
+        {
+          storage.streams.map(s => <Stream content={s} key={s.link}/>)
+        }
+      </div>
     </div>
   );
 };
