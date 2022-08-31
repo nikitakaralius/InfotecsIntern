44f00cb1757b8dac6f3e182eb54be4d7b3491e7a
nikitakaralyus <nikitakaralyus@gmail.com>
2022-08-31 12:00:44 +0300

    Created rss feed parser

---
+++ src/RssAggregator/client-app/src/services/rssParser.ts
@@ -0,0 +1,39 @@
+import {IArticle, String100, String250} from '../domain';
+
+type MapArticle = (element: Element) => IArticle;
+
+const mapArticle: MapArticle = (element) => {
+  const title = element.querySelector('title')!.textContent!;
+  const description = element.querySelector('description')!.textContent!;
+  const pubDate = element.querySelector('pubDate')!.textContent!;
+  const link = element.querySelector('link')!.textContent!;
+
+  return {
+    title: String100.createClamped(title),
+    description: String250.createClamped(description),
+    link: new URL(link),
+    pubDate: new Date(pubDate)
+  };
+};
+
+const createXmlTree = (content: string) => {
+  const parser = new DOMParser();
+  return parser.parseFromString(content, 'application/xml');
+};
+
+const parseFeed = (content: string) => {
+  const tree = createXmlTree(content);
+  try {
+    const channel = tree.querySelector('channel')!;
+    const articleList = channel.querySelectorAll('item')!;
+
+    let feed: IArticle[] = [];
+    articleList.forEach(a => feed.push(mapArticle(a)));
+
+    return feed;
+  } catch (e) {
+    throw new Error('Unsupported RSS dialect');
+  }
+};
+
+export {parseFeed};
