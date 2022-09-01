5b134e602b2e1aa2199bdbdb332eb7329699433f
nikitakaralyus <nikitakaralyus@gmail.com>
2022-09-01 17:49:22 +0300

    Created Modal component

---
+++ src/RssAggregator/client-app/src/components/modal/Modal.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Modal.module.scss';
+import {createPortal} from 'react-dom';
+
+interface IModalProps {
+  children: React.ReactNode;
+  open: boolean;
+  onClose: () => void;
+}
+
+const Modal = ({children, open}: IModalProps) => {
+  if (!open) return null;
+  return createPortal(
+    <>
+      <div className={styles.overlay}/>
+      <div className={styles.content}>
+        {children}
+      </div>
+    </>,
+    document.getElementById('modal')!
+  );
+};
+
+export default Modal;
